31a77f6069c8a03d13fb93b31682ac24dd4c6679
SergioRojasUtec <sergio.rojas@utec.edu.pe>
2017-04-10 13:22:23 -0500

    Create numeral.cc

---
+++ numeral.cc
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "numeral.h"
+using namespace std;
+
+int num(int );
+int main()
+{
+    cout<<"Numeral"<<endl;cout<<"Ingresa un numero : "<<endl;
+  int b;
+  cin>>b;
+    cout<<num(b);
+    
+    
+}
